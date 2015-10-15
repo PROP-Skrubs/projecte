b4fbc5fe403b0014e8a2140c04072e38ca83edad
Gabberland <oriol.giralt@yahoo.es>
2015-10-15 17:33:54 +0200

    Afegit controlador partida

---
+++ src/ControladorPartida.java
@@ -0,0 +1,16 @@
+/**
+ * Created by oriol on 15/10/15.
+ */
+
+public class ControladorPartida
+{
+    public void posar_numero(){}
+    public boolean validar_progres(){
+
+        return false;
+    }
+
+
+
+    //public Hidato crearHidatoAmbParametres(){}
+}
