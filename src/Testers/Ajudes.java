2608cd4733931f8104024c35536b2e2967fad9a4
Nalaboo <maria.vives@est.fib.upc.edu>
2015-11-15 10:58:28 +0100

    Update Ajudes.java

--- src/Testers/Ajudes.java
+++ src/Testers/Ajudes.java
@@ -16,25 +16,28 @@ public class Ajudes {
         ControladorAjudes ca = new ControladorAjudes();
         Casella c = new Casella();
         c = ca.help1(h);
-        System.out.println("Jo de tu posaria el numero" + c.elem + " a les posicions x:" + c.x + " y:" + c.y);
+        System.out.println("Jo de tu posaria el numero" + c.elem + " a les posicions x:" + (c.x+1) + " y:" + (c.y+1));
     }
     public void ajuda2(Hidato h){
         ControladorAjudes ca = new ControladorAjudes();
         Scanner capt = new Scanner(System.in);
         System.out.println("introdueix X:");
         int x = capt.nextInt();
-        while (x < 1 | x > h.getTauler().getLongitud()) {
+        while (x < 1 | x > 6) {
             System.out.println("X no valida, introdueix una altra");
             x = capt.nextInt();
         }
 
         System.out.println("introdueix Y:");
         int y = capt.nextInt();
-        while (y < 1 | y > h.getTauler().getLongitud()) {
+        while (y < 1 | y > 6) {
             System.out.println("Y no valida, introdueix una altra");
             y = capt.nextInt();
         }
         List<Integer> l = ca.help2(x, y, h);
+
+        if(l.isEmpty()) System.out.print("Estoy vacia");
+
         Iterator<Integer> it = l.iterator();
         System.out.println("Possibles Candidats:");
         while (it.hasNext()) {
