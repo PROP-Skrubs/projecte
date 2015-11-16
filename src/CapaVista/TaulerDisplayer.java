620a7192d1eed2ac2ea4dd269036dfce335b2e29
Daniel Armengod <d.armengod@gmail.com>
2015-11-16 09:03:51 +0100

    Explorat com fer els numeros editables. Passo a acabar el GestorPartida

--- src/CapaVista/TaulerDisplayer.java
+++ src/CapaVista/TaulerDisplayer.java
@@ -33,7 +33,6 @@ public class TaulerDisplayer extends JPanel
                 numerosAlTauler[i][j] = new JLabel();
                 numerosAlTauler[i][j].setFont(numerosAlTauler[i][j].getFont().deriveFont(32.0f));
                 add(numerosAlTauler[i][j]);
-
             }
         }
         g.setHgap(5);
@@ -69,16 +68,17 @@ public class TaulerDisplayer extends JPanel
         numerosAlTauler[c.x][c.y].setText(Integer.toString(c.elem));
     }
 
-        public static void main(String[] args)
-        {
-            JFrame f = new JFrame();
-            TaulerDisplayer t = new TaulerDisplayer(10);
-            f.setContentPane(t);
-            for (int i = 0; i < 10; ++i)
-                for (int j = 0; j < 10; ++j)
-                    t.setCasella(new Casella(i,j,i+1));
-            f.pack();
-            f.setVisible(true);
-        }
+
+    public static void main(String[] args)
+    {
+        JFrame f = new JFrame();
+        TaulerDisplayer t = new TaulerDisplayer(10);
+        f.setContentPane(t);
+        for (int i = 0; i < 10; ++i)
+            for (int j = 0; j < 10; ++j)
+                t.setCasella(new Casella(i, j, i + 1));
+        f.pack();
+        f.setVisible(true);
+    }
 
 }
