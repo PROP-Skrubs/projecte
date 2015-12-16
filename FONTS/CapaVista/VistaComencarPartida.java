6792fa0ae050f7c5267868ceff9be50579566e8d
Eduard <edujseoane@gmail.com>
2015-12-16 20:58:38 +0100

    baenŕklea

--- FONTS/CapaVista/VistaComencarPartida.java
+++ FONTS/CapaVista/VistaComencarPartida.java
@@ -101,7 +101,7 @@ public class VistaComencarPartida extends DialogGeneric
         java.util.List<Integer> totesID;
         if (quinModel == 1) //Partida
         {
-            totesID = GestorPartida.donaTotesIDPartidaDeUsuari(ControladorLogin.getUsuariActual().getUniqID());
+            totesID = ControladorPartida.donarPartidaSegonsUsuari(ControladorLogin.getUsuariActual().getUniqID());
         }
         else
         {
