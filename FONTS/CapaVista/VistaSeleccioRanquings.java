6b055c7a2e5d0c821c244a5e64da88a7e26c78a6
Eduard <edujseoane@gmail.com>
2015-12-17 00:17:14 +0100

    grnaeg

--- FONTS/CapaVista/VistaSeleccioRanquings.java
+++ FONTS/CapaVista/VistaSeleccioRanquings.java
@@ -2,6 +2,8 @@ package CapaVista;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by daniel on 16/12/15.
@@ -11,7 +13,12 @@ public class VistaSeleccioRanquings extends DialogGeneric
     private GridBagLayout layout;
     private JRadioButton radioButtonHidato;
     private JRadioButton radioButtonDificultat;
+    private JRadioButton radioButtonCopsResolts;
     private JList listSeleccions;
+    private ButtonGroup radioButtonGroup;
+    private AbstractListModel<String> modelLlistaHidatos;
+    private AbstractListModel<String> modelLlistaDificultat;
+    private AbstractListModel<String> modelLlistaCopsResolsts;
 
 
     public VistaSeleccioRanquings()
@@ -43,6 +50,12 @@ public class VistaSeleccioRanquings extends DialogGeneric
         c.gridy = 1;
         mainPanel.add(toAdd, c);
 
+        toAdd = radioButtonCopsResolts = new JRadioButton("Veure ranquings de millors hidatos per cops resolts");
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 2;
+        mainPanel.add(toAdd, c);
+
         toAdd = listSeleccions = new JList();
         listSeleccions.setPreferredSize(new Dimension(150,150));
         c = new GridBagConstraints();
@@ -51,11 +64,43 @@ public class VistaSeleccioRanquings extends DialogGeneric
         c.gridy = 0;
         c.weighty=2;
         mainPanel.add(toAdd, c);
+
+        radioButtonGroup = new ButtonGroup();
+        radioButtonGroup.add(radioButtonHidato);
+        radioButtonGroup.add(radioButtonDificultat);
+        radioButtonGroup.add(radioButtonCopsResolts);
+
     }
 
     private void afegirActionListeners()
     {
-
+        radioButtonDificultat.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                listSeleccions.setModel(modelLlistaDificultat);
+                listSeleccions.setSelectedIndex(0);
+            }
+        });
+        radioButtonHidato.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                listSeleccions.setModel(modelLlistaHidatos);
+                listSeleccions.setSelectedIndex(0);
+            }
+        });
+        radioButtonCopsResolts.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                listSeleccions.setModel(modelLlistaCopsResolsts);
+                listSeleccions.setSelectedIndex(0);
+            }
+        });
     }
 
     @Override
